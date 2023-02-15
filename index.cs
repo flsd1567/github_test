bd1f710db57fa7d399abe86a3bbb938d051ef965
flsd1567 <flsd1567@gmail.com>
2023-02-15 13:10:14 +0900

    Update Program Style

--- index.cs
+++ index.cs
@@ -1,2 +1,8 @@
-Console.WriteLine("Hello World");
-Console.WriteLine("Hi Worild");
+Console.WriteLine("Hi Worild");Console.WriteLine("Hello World");internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World");
+        Console.WriteLine("Hi Worild");
+    }
+}
